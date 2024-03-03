9c80033ca5a882956379a6b981dde1402ea168d0
KojieSann <109472332+KojieSann@users.noreply.github.com>
2024-03-03 10:36:01 +0800

    Add files via upload

---
+++ phpWebSystem/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+<div class="container">
+    <div class="wrapper"> 
+    <form action="" method="post">
+            <div class="info">
+                <h1>CRUD Operations</h1>
+            </div>
+            <div class="input-container">
+                <div class="input-box">
+                <input type="text" required class="firstname" name="firstName"/>
+                <span>Firstname</span>
+                </div>
+                <div class="input-box">
+                <input type="text" required class="lastname" name="lastName"/>
+                <span>Lastname</span>
+                </div>
+            </div>
+            <button type="submit" class="submit">
+                Submit
+            </button>
+    </form>
+    </div>
+</div>
+</body>
+</html>
